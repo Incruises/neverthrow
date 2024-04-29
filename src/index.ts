83582ccb9a2e2c31eb9a1065b3385b0d7a698f3a
Kenneth <kennysmoothx@gmail.com>
2024-04-29 11:40:05 -0400

    maythrow

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Result, ok, err } from './result'
 export { Result, ok, Ok, err, Err, fromThrowable, safeTry } from './result'
 export {
   ResultAsync,
@@ -7,3 +8,13 @@ export {
   fromPromise,
   fromSafePromise,
 } from './result-async'
+
+export function mt<T, U>(promise: Promise<T>): Promise<Result<T, U>> {
+	return promise
+		.then<Result<T, U>>((data: T) => ok(data))
+		.catch<Result<T, U>>((error: U) => {
+			return err(error);
+		});
+}
+
+export const maythrow = mt;
